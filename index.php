cb365f03a51a28476d93b2b26d1ec4737d6369bd
rustyralminar <alminar.rusty.r@gmail.com>
2022-02-05 22:59:02 +0800

    up

---
+++ index.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css">
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js"></script>
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css" crossorigin="anonymous">
+</head>
+<body>
+	<?php include 'nav.php'; ?>
+	  <!-- Page content holder -->
+<div class="jumbotron jumbotron-fluid p-4"><br>
+	<div align="center">
+		<h4 align="C">EP</h4>
+ 		<h1>10000</h1>
+	</div>
+</div>
+	<div class="container"  align="center">
+ 		<button type="button" class="btn btn-primary">EP Grocery</button>
+		<button type="button" class="btn btn-primary">...</button>
+		<button type="button" class="btn btn-primary">...</button>
+
+		<hr>
+
+<div id="demo" class="carousel slide" data-ride="carousel" > 
+
+  <!-- Indicators -->
+  <ul class="carousel-indicators">
+    <li data-target="#demo" data-slide-to="0" class="active"></li>
+    <li data-target="#demo" data-slide-to="1"></li>
+    <li data-target="#demo" data-slide-to="2"></li>
+  </ul>
+
+  <!-- The slideshow -->
+  <div class="carousel-inner" style="max-height:200px;">
+    <div class="carousel-item active">
+      <img src="https://www.w3schools.com/bootstrap4/la.jpg" alt="Los Angeles">
+    </div>
+    <div class="carousel-item">
+      <img src="https://www.w3schools.com/bootstrap4/chicago.jpg" alt="Chicago">
+    </div>
+    <div class="carousel-item">
+      <img src="https://www.w3schools.com/bootstrap4/ny.jpg" alt="New York">
+    </div>
+  </div>
+
+  <!-- Left and right controls -->
+  <a class="carousel-control-prev" href="#demo" data-slide="prev">
+    <span class="carousel-control-prev-icon"></span>
+  </a>
+  <a class="carousel-control-next" href="#demo" data-slide="next">
+    <span class="carousel-control-next-icon"></span>
+  </a>
+
+</div>
+<hr>
+	<h5>About Us</h5>
+	<h5 class="text-secondary">Lorem ipsum, dolor, sit amet consectetur adipisicing elit. Quidem atque sed soluta quaerat rerum qui maxime ab, voluptatem quisquam, commodi. In, voluptas iusto. Facilis ullam omnis officia, quod tenetur quidem?</h5>
+	<a href="">More Info</a>
+	</div>
+
+
+
+
+
+</body>
+</html>
---
+++ nav.php
@@ -0,0 +1,21 @@
+<nav class="fixed-bottom">
+<div class="d-flex mx-auto" align="center"  style="height: 50px;">
+
+  <a href="" class="p-2 bg-success flex-fill nav-link  text-light">
+    <i class="fas fa-home"></i> HOME
+  </a>
+
+  <a href="" class="p-2 bg-success flex-fill nav-link  text-light">
+    <i class="fas fa-qrcode"></i> QR
+  </a>
+
+  <a href="" class="p-2 bg-success flex-fill nav-link  text-light">
+    <i class="fas fa-stream"></i> EXCHANGE
+  </a>
+
+  <a href="" class="p-2 bg-success flex-fill nav-link  text-light">
+    <i class="fas fa-home"></i> PROFILE
+  </a>
+</div>
+</nav>
+
